bbb739b3c20aa445da0cb9c294d702296adc05b3
Robin <97387960+robeecodes@users.noreply.github.com>
2024-12-01 19:08:51 +0000

    fix: add .gitignore

--- 1_Historian Hysteria/Challenge_One/Challenge_One/Program.cs
+++ 1_Historian Hysteria/Challenge_One/Challenge_One/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-
-namespace _1_Historian_Hysteria;
+﻿namespace Challenge_One;
 
 class Program {
     static void Main() {

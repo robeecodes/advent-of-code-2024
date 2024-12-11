edc6bb3202aec5f994fc7ccc3b8e64db086853cf
Robin <97387960+robeecodes@users.noreply.github.com>
2024-12-11 23:39:58 +0000

    feat: complete day 11

---
+++ 11_Plutonian Pebbles/ChallengeOne/ChallengeOne/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ChallengeOne {
+    class Program {
+        public static void Main(string[] args) {
+            var input = ReadInput();
+
+            for (int i = 0; i < 25; i++) {
+                Console.WriteLine(i);
+                Blink(input);
+            }
+            
+            Console.WriteLine(input.Count);
+        }
+
+        private static List<long> ReadInput() {
+            string[] text = File.ReadAllText("Inputs/inputs.txt").Split(' ');
+
+            return text.Select(item => long.Parse(item)).ToList();
+        }
+
+        private static void Blink(List<long> input) {
+            int i = 0;
+            while (i < input.Count) {
+                if (input[i] == 0) input[i] = 1;
+                else if (input[i].ToString().Length % 2 == 0) {
+                    string tmp = input[i].ToString();
+                    string tmpL = tmp.Substring(0, tmp.Length / 2);
+                    string tmpR = tmp.Substring(tmp.Length / 2, tmp.Length / 2);
+                    input[i] = long.Parse(tmpL);
+                    input.Insert(i + 1, long.Parse(tmpR));
+                    i++;
+                }
+                else input[i] *= 2024;
+
+                i++;
+            }
+            
+            // foreach (long num in input) {
+            //     Console.Write(num + ", ");
+            // }
+            // Console.WriteLine();
+        }
+    }
+}
---
+++ 11_Plutonian Pebbles/ChallengeTwo/ChallengeTwo/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ChallengeTwo {
+    class Program {
+        public static void Main(string[] args) {
+            var input = ReadInput();
+
+            for (int i = 0; i < 75; i++) {
+                Blink(input);
+            }
+            
+            Console.WriteLine(input.Values.Sum());
+        }
+
+        private static Dictionary<long, long> ReadInput() {
+            string[] text = File.ReadAllText("Inputs/inputs.txt").Split(' ');
+            var input = new Dictionary<long, long>();
+
+            foreach (var item in text) {
+                if (!input.TryAdd(long.Parse(item), 1)) input[long.Parse(item)]++;
+            }
+
+            return input;
+        }
+
+        private static void Blink(Dictionary<long, long> input) {
+            var updates = new Dictionary<long, long>();
+
+            foreach (var key in input.Keys.ToList()) {
+                long count = input[key];
+                if (count == 0) continue;
+
+                if (key == 0) {
+                    if (!updates.TryAdd(1, count)) updates[1] += count;
+                }
+                else if (key.ToString().Length % 2 == 0) {
+                    string tmp = key.ToString();
+                    int mid = tmp.Length / 2;
+                    long tmpL = long.Parse(tmp.Substring(0, mid));
+                    long tmpR = long.Parse(tmp.Substring(mid));
+
+                    if (!updates.TryAdd(tmpL, count)) updates[tmpL] += count;
+                    if (!updates.TryAdd(tmpR, count)) updates[tmpR] += count;
+                }
+                else {
+                    long newKey = key * 2024;
+                    if (!updates.TryAdd(newKey, count)) updates[newKey] += count;
+                }
+
+                input[key] = 0;
+            }
+
+            foreach (var kvp in updates) {
+                if (!input.TryAdd(kvp.Key, kvp.Value)) input[kvp.Key] += kvp.Value;
+            }
+        }
+    }
+}
